1625bc0383fa434b714f755870ae5a2a6e536dac
alfianazizi <alfianazizi4869@gmail.com>
2018-05-30 01:00:17 +0700

    revision script

--- bash_script/prepare_flask.sh
+++ bash_script/prepare_flask.sh
@@ -3,5 +3,19 @@
 # Prepare system for Web Dashboard Flask
 #
 
+read -p "User Proxy: " user
+read -p "Password Proxy: " pass
+
+export http_proxy="http://$user:$pass@cache.itb.ac.id:8080"
+export https_proxy="http://$user:$pass@cache.itb.ac.id:8080"
+
 echo "Install python-pip package"
 sudo apt-get install python-pip
+
+echo "Install Flask-WTF module"
+sudo -E pip install flask-wtf
+
+echo "Install Flask-CORS module"
+sudo -E pip install flask-cors
+
+echo " System Ready "
