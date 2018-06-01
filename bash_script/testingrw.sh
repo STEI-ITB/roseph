cca3d4f23124c61cb5cc11163334226a219c166b
alfianazizi <alfianazizi4869@gmail.com>
2018-06-01 23:22:44 +0700

    Update testingrw.sh

--- bash_script/testingrw.sh
+++ bash_script/testingrw.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-#
-# How to use:
-#   chmod +x samba-access.sh
-#   ./samba-S
-#
-#
-#
-
-
-#if [ -z "$1" ];then
-#  echo "How to use this script?"
-#  echo "bash the script"
-#  exit 0
-#fi
 
 read -p "Masukkan pool yang ingin di test " pool
 read -p "Masukkan berama lama waktu test satuan (satuan sekon)" waktu
